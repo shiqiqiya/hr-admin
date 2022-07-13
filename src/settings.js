f486a433bb96a7b1d57f0418cb4f9336a0d69696
shiqiqi <2418687931@qq.com>
2022-07-13 11:51:03 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '人力资源管理后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 是否让 header 是不是固定定位
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 是否在侧边栏上面显示logo
+   */
+  sidebarLogo: true
+}
